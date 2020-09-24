712a9bdb9dc56f6b381f555b4e2706988d5d36d3
Reggie Minor <37523930+rminor1022@users.noreply.github.com>
2020-09-24 00:00:45 -0500

    Update TwitterClient.java

--- SimpleTweet/app/src/main/java/com/codepath/apps/restclienttemplate/TwitterClient.java
+++ SimpleTweet/app/src/main/java/com/codepath/apps/restclienttemplate/TwitterClient.java
@@ -24,8 +24,8 @@ import com.github.scribejava.core.builder.api.BaseApi;
 public class TwitterClient extends OAuthBaseClient {
 	public static final BaseApi REST_API_INSTANCE = TwitterApi.instance();
 	public static final String REST_URL = "https://api.twitter.com/1.1";
-	public static final String REST_CONSUMER_KEY = BuildConfig.CONSUMER_KEY;       // Change this inside apikey.properties
-	public static final String REST_CONSUMER_SECRET = BuildConfig.CONSUMER_SECRET; // Change this inside apikey.properties
+	public static final String REST_CONSUMER_KEY = CONSUMER_KEY;       // Change this inside apikey.properties
+	public static final String REST_CONSUMER_SECRET = CONSUMER_SECRET; // Change this inside apikey.properties
 
 	// Landing page to indicate the OAuth flow worked in case Chrome for Android 25+ blocks navigation back to the app.
 	public static final String FALLBACK_URL = "https://codepath.github.io/android-rest-client-template/success.html";
